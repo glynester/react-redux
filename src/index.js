632643c8851b0725025f09d5f05278d9c0d91c55
Gee <glynester@hotmail.com>
2023-01-08 16:06:28 +0000

    Some notes added for redux chrome webdev tools.

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import { createStore } from 'redux';
 import allReducers from './reducers';
-import { Provider } from 'react-redux'; // Connects global state to the entire app - wrap the whole app with this "component".
+import { Provider } from 'react-redux'; // Connects global state to the entire app - wrap the whole app with this "component" - see below.
 
 // const store = createStore(counterReducer, isLoggedReducer); // doesn't work. Need to combine first with "combineReducers".
 const myStore = createStore(
   allReducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()    // Add this line to aid with development - also need chrome extension added:  https://chrome.google.com/webstore/detail/redux-devtools/lmhkpmbekcpmknklioeibfkpmmfibljd 
+  // "Redux" tab in chrome developer tools will show your app's state.
 )
 
 
